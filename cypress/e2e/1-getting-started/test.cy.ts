4348b52d3cc12d3b8a158e516773376853f61a1b
Mani Movassagh <mani.mgh@gmail.com>
2024-04-14 16:03:52 +0200

    tt

--- cypress/e2e/1-getting-started/test.cy.ts
+++ cypress/e2e/1-getting-started/test.cy.ts
@@ -3,7 +3,11 @@
 describe('first test to be ', () => {
 
 
-  it('test it', () => {
+  it('test it tttt', () => {
+    cy.visit("google.com")
+    cy.go("back")
+  })
+  it('second one it for that', () => {
     cy.visit("google.com")
     cy.go("back")
   })
--- cypress/utilities/list_maker/lister.go
+++ cypress/utilities/list_maker/lister.go
@@ -10,9 +10,44 @@ import (
 	"strings"
 )
 
+func List_Maker() {
+	// Define the directory to search
+	directory := "../../../cypress/e2e"
+
+	// Run the command to list Cypress test files
+	cmd := exec.Command("find", directory, "-name", "*.cy.*")
+	out, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println("Error listing Cypress test files:", err)
+		return
+	}
+
+	// Print the list of Cypress test files
+	fmt.Println("Cypress test files:")
+	fmt.Println(string(out))
+
+	// Save the list to a text file
+	filePath := "cypress_test_files.txt"
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(string(out))
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("Cypress test files list saved to:", filePath)
+}
+
 func main() {
 	// Define the directory to search
-	directory := "../../cypress/e2e"
+	directory := "../../../cypress/e2e"
 
 	// Run the command to list Cypress test files
 	cmd := exec.Command("find", directory, "-name", "*.cy.*")
@@ -90,6 +125,6 @@ func main() {
 
 	fmt.Println("Cypress describe function strings saved to:", jsonFilePath)
 
-
 	fmt.Println("----------------------------------")
+	List_Maker()
 }
