9ca77ba7ce2ee9601df003cf7f5373e497bde5b1
Mani Movassagh <mani.mgh@gmail.com>
2024-04-30 23:15:19 +0200

    tt

--- cypress/e2e/3-news/mani.cy.ts
+++ cypress/e2e/3-news/mani.cy.ts
@@ -16,7 +16,5 @@ describe('test lister', () => {
        cy.get("a").click()
        cy.get("a").click()
        cy.get("a").click()
-       cy.get("a").click()
-       cy.get("a").click()
     });
  })
