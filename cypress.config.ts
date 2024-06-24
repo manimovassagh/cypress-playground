8ff5687b3f49ddd2e7c877a15f869a58cc160cf9
Mani Movassagh <mani.mgh@gmail.com>
2024-06-25 01:34:03 +0200

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      config.env.JWT_TOKEN = process.env.JWT_TOKEN;
+
       // implement node event listeners here
     },
   },
