c48058f57a5f43e5b5826c7cc73634f88eff3637
Mani Movassagh <mani.mgh@gmail.com>
2024-06-25 01:43:03 +0200

    test

--- cypress/e2e/3-news/mani.cy.ts
+++ cypress/e2e/3-news/mani.cy.ts
@@ -1,6 +1,8 @@
 describe('Log JWT Token', () => {
    it('should log the JWT token', () => {
+      const jwt='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
      const jwtToken = Cypress.env('JWT_TOKEN');
+     expect(jwtToken).to.equal(jwt);
      cy.log(`JWT Token: ${jwtToken}`);
    });
  });
