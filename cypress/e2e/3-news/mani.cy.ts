4b4b6441b45246a82d67870446bce8b6535b7446
Mani Movassagh <mani.mgh@gmail.com>
2024-06-25 02:14:58 +0200

    latesttt

--- cypress/e2e/3-news/mani.cy.ts
+++ cypress/e2e/3-news/mani.cy.ts
@@ -4,33 +4,14 @@
    it('should log the JWT token', () => {
       const jwt='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 
-     const cypressJwtToken = Cypress.env('CYPRESS_JWT');
-     const jwtToken = Cypress.env('JWT_TOKEN');
-     const jt2 = jwtToken
-     expect(cypressJwtToken).to.be.a('string');
-     expect(jwtToken).to.be.a('string')
-     expect(jwtToken).to.equal(jt2);
+     const cypressJwtToken = Cypress.env('JWT_TOKEN');
+ 
+console.log(cypressJwtToken)
 
-     const data = {
-      CYPRESS_JWT_TOKEN: cypressJwtToken,
-      JWT_TOKEN: jwtToken
-    };
 
-    cy.writeFile('cypress/fixtures/env.json', data).then(() => {
-      cy.log('Environment variables written to env.json');
-    });
-     cy.log(`CYPRESS_JWT_TOKEN: ${cypressJwtToken ? cypressJwtToken.substring(0, 4) + '****' : 'undefined'}`);
-     cy.log(`JWT_TOKEN: ${jwtToken ? jwtToken.substring(0, 4) + '****' : 'undefined'}`);
-   });
  });
 
 
+})
 
- describe('first', () => { 
-   it('test reader', () => {
-      cy.readFile('cypress/fixtures/env.json').then((data) => {
-         cy.log('Environment variables written to env.json',data);
-       });
-   });
-  })
  
