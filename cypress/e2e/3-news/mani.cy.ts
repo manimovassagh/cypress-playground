1217448ac6dcc5acbd3c248efb4fe6859fb990d1
Mani Movassagh <mani.mgh@gmail.com>
2024-06-25 02:20:57 +0200

    runner

--- cypress/e2e/3-news/mani.cy.ts
+++ cypress/e2e/3-news/mani.cy.ts
@@ -1,16 +1,10 @@
+describe("Log JWT Token 22222", () => {
+  it("should log the JWT token", () => {
+    const jwt =
+      "2eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
+    const cypressJwtToken = Cypress.env("JWT_TOKEN");
 
- describe('Log JWT Token 22222', () => {
-   it('should log the JWT token', () => {
-      const jwt='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-
-     const cypressJwtToken = Cypress.env('JWT_TOKEN');
- 
-cy.wrap(jwt).as('jwtToken').should("equal",cypressJwtToken);
-
- });
-
-
-})
-
- 
+    cy.wrap(jwt).as("jwtToken").should("equal", cypressJwtToken);
+  });
+});
